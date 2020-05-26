c2b9ff33c006ad00e0c72292e4ce8c874ccab25b
Guillaume Duveau <guillaume.duveau@gmail.com>
2020-05-26 13:38:35 +0200

    max resolution

--- phantomjs.test.js
+++ phantomjs.test.js
@@ -4,8 +4,8 @@ const test = async () => {
   const instance = await phantom.create()
   const page = await instance.createPage()
   page.property('viewportSize', {
-    width: 1920,
-    height: 1080
+    width: 1011,
+    height: 768
   })
   const openStatus = await page.open('https://github.com')
   const renderStatus = await page.render('test.png')
